e055fe59583d01aa3c085296bc2536fedca4ca52
Christina Petersen <christina.h.petersen4@gmail.com>
2021-03-19 20:04:29 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -28,9 +28,10 @@ function ternaryTeenager(age) {
 
 function switchAge(age) {
   switch(age) {
-    case (age >= 13 && age <= 19):
+    case (age >= 13):
       return 'You are a teenager';
       break;
+    case (age <=19):
     default:
       return 'You have an age';
   }
